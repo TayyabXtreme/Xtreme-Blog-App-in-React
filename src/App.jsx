acc3741d9f646b43f89af112d318fac397830ebd
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-06-17 14:35:39 +0500

    redux config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { useEffect } from 'react'
+import { login ,logOut} from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env)
-  
 
-  return (
-    <>
-     <h1>A blog app with appwrite</h1>
-    </>
-  )
+  const [loading,setLoading] = useState(true)
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logOut())
+      }
+
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+     <div className='w-full block'>
+      <Header/>
+      <main>
+        Todo Outlet
+        {/* <Outlet/> */}
+      </main>
+      <Footer/>
+     </div>
+    </div>
+  ):<h1>null</h1>
 }
 
 export default App
---
+++ src/store/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    status:false,
+    userData:null
+}
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+
+        login:(state,action)=>{
+            state.status=true;
+            state.userData=action.payload.userData;
+        },
+
+        logOut:(state,action)=>{
+            state.status=false;
+            state.userData=null;
+        }
+
+
+    }
+
+});
+
+export const {login,logOut}=authSlice.actions;
+
+export default authSlice.reducer;
